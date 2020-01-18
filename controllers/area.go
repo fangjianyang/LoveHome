399b03eac3f456933248150f260828df2cb4405f
JianYangF <fangjianyang_main@163.com>
2020-01-18 14:08:47 +0800

    完善登录和redis缓冲模块

--- controllers/area.go
+++ controllers/area.go
@@ -4,7 +4,11 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"LoveHome/models"
-	//"encoding/json"
+	_"github.com/astaxie/beego/cache/redis"
+	"github.com/astaxie/beego/cache"
+	"time"
+	"encoding/json"
+	"fmt"
 )
 
 type AreaController struct {
@@ -24,15 +28,35 @@ func (c *AreaController) GetArea() {
     resp["errmsg"] = "success"
     defer c.RetData(resp)
 
-    // 1 get data from session
+
+    cacheHandel, err := cache.NewCache("redis", `{"key":"lovehome","conn":":6379","dbNum":"0"}`)
+    if err != nil{
+        resp["errno"] =12110
+        resp["errmsg"] = "query err"
+        return
+    }
+
+
+    // 1 get data from cache,mush Ummarshal from json to the right fmt
+    if areaData := cacheHandel.Get("area"); areaData != nil{
+        var areas  []models.Area
+        err1 := json.Unmarshal(areaData.([]byte), &areas)
+        if err1 != nil{
+            beego.Error("Ummarshal data from redis failed!")
+            return
+        }
+        beego.Info("areaData : " , areas)
+        resp["data"] = areas
+        return
+    }
 
     // 2 get data from db
     var area  []models.Area
     ormHandel := orm.NewOrm()
 
     //err := ormHandel.Read(&area)
-    num,err := ormHandel.QueryTable("area").All(&area)
-    if err != nil{
+    num,errDb := ormHandel.QueryTable("area").All(&area)
+    if errDb != nil{
         beego.Info("query data from db error!")
 
         resp["errno"] =12110
@@ -50,8 +74,20 @@ func (c *AreaController) GetArea() {
 
 
     resp["data"] = area
-    //beego.Info("the data from mysql is ",area)
+    beego.Info("the data from mysql is ",area)
 
     // 3 paking data as json return to client
+    jsonStr,errFmt := json.Marshal(area)
+    if errFmt != nil {
+        beego.Info("encoding err")
+        return
+    }
+    //beego.Info("JsonStr : ",jsonStr)
+    fmt.Printf("JsonStr %s",jsonStr)
+    errFmt = cacheHandel.Put("area",jsonStr,time.Second * 3600)
+    if errFmt != nil{
+        beego.Error("cache err")
+        return
+    }
 
 }
--- controllers/session.go
+++ controllers/session.go
@@ -2,10 +2,10 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
-	// github.com/astaxie/beego/session
-	// "github.com/astaxie/beego/orm"
+	// "github.com/astaxie/beego/session"
+	 "github.com/astaxie/beego/orm"
 	 "LoveHome/models"
-	// "encoding/json"
+	 "encoding/json"
 )
 
 type SessionController struct {
@@ -25,7 +25,7 @@ func (c *SessionController) RetData(resp map[string]interface{}){
 func (this *SessionController) GetSessionData() {
     resp := make(map[string]interface{})
     // this.MkRet(models.RECODE_OK,resp)
-    // 太奇葩了，加入这个errcode = 0 界面就没有按钮
+    // 太奇葩了，if 这个errcode = 0 界面就没有按钮
     resp["errno"] = models.RECODE_DATAERR
     resp["errmsg"] = models.RecodeText(models.RECODE_DATAERR)
     defer this.RetData(resp)
@@ -48,4 +48,50 @@ func  (this *SessionController) DeleteSession() {
     this.DelSession("name")
     resp["errno"] = models.RECODE_OK
     resp["errmsg"] = models.RecodeText(models.RECODE_OK)
+}
+
+func (this *SessionController) Login(){
+    resp := make(map[string]interface{})
+    resp["errno"] =0
+    resp["errmsg"] = "success"
+    defer this.RetData(resp)
+
+    err1 := json.Unmarshal(this.Ctx.Input.RequestBody, &resp)
+    if err1 != nil{
+        resp["errno"] =12345
+        resp["errmsg"] = "requestBody error"
+        beego.Info("Unmarshal error")
+        return
+    }
+    beego.Info(resp)
+    // get user data
+    ormHandel := orm.NewOrm()
+    user := models.User{Name:resp["mobile"].(string)}
+    userData := ormHandel.QueryTable("user");
+    err := userData.Filter("mobile", resp["mobile"].(string)).One(&user)
+
+    if err != nil{
+        resp["errno"] =12345
+        resp["errmsg"] = "requestBody error"
+        return
+    }
+
+    // judge if this data is valid
+
+    // judge if the data is right
+    beego.Info(user)
+    if user.Password_hash != resp["password"] {
+        resp["errno"] =12345
+        resp["errmsg"] = "requestBody error"
+        return
+    }
+
+    // add session
+    this.SetSession("name",resp["mobile"].(string))
+    this.SetSession("mobile",resp["mobile"].(string))
+    this.SetSession("user_id",user.Id)
+
+    // return result
+    resp["errno"] =0
+    resp["errmsg"] = "success"
 }
