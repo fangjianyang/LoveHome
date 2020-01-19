097ecb21b3ba07cc907ea33ad40f32e272c07bc9
JianYangF <fangjianyang_main@163.com>
2020-01-19 11:48:10 +0800

    完善头像传输功能，此处用到了fdfa和第三方库

--- routers/router.go
+++ routers/router.go
@@ -12,5 +12,7 @@ func init() {
     beego.Router("/api/v1.0/session",&controllers.SessionController{},"get:GetSessionData;delete:DeleteSession")
     beego.Router("/api/v1.0/users",&controllers.UserController{},"post:Reg")
     beego.Router("/api/v1.0/sessions",&controllers.SessionController{},"post:Login")
+    beego.Router("/api/v1.0/user/avatar",&controllers.UserController{},"post:PostAvatar")
+
 
 }
